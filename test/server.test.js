4db3f2257c00b7d8dc2fa6966db10a07d5bcf8c8
Arfon Smith <arfon@github.com>
2014-07-17 16:52:58 -0700

    Running specs

--- test/server.test.js
+++ test/server.test.js
@@ -1,6 +1,11 @@
 var request = require('supertest')
   , app = require('../server');
 
+var fs = require('fs');
+var exec = require('child_process').execFile;
+
+var logfmt = require("logfmt");
+
 var chai = require('chai');
 var assert = chai.assert;
 
@@ -32,6 +37,21 @@ describe('Math JS Routes', function() {
           return done(err);
         var result = res.text;
         assert.include(result, '<!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">')
+        done();
+      });
+    });
+  });
+
+  describe('BENCHMARK MathJax with some TeX', function() {
+    it('should return an SVG', function(done) {
+      file = fs.readFileSync('./test/fixtures/performance/1.tex', 'utf-8');
+      var output = exec('./node_modules/MathJax-node/bin/tex2svg', [file], function (error, stdout, stderr) {
+        if (error != null) {
+          console.log(process.cwd());
+          console.log(error);
+          done();
+        }
+        assert.include(stdout, '<!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">')
         done();
       });
     });
